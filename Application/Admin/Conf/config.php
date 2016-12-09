8d3a11dffbd6a9cd100b31ef1eb8aa51e381e825
hmn <925203783@qq.com>
2016-12-09 18:59:24 +0800

    修改用户查询功能

--- Application/Admin/Conf/config.php
+++ Application/Admin/Conf/config.php
@@ -3,5 +3,5 @@ return array(
 	//'配置项'=>'配置值'
     //主题静态文件路径
     'TMPL_PARSE_STRING' => array(
-        '__STATIC__' => __ROOT__.'/Application/'.MODULE_NAME.'/View/' . '/Public/static',),
+        '__STATIC__' => __ROOT__.'/Application/'.MODULE_NAME.'/View' . '/Public/static',),
 );
