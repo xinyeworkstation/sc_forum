f9a137bee6498cfce31131c9dda464d32e59fb32
zyq <1378622586@qq.com>
2016-12-14 20:38:24 +0800

    verify

--- Application/Admin/Controller/WorkController.class.php
+++ Application/Admin/Controller/WorkController.class.php
@@ -38,23 +38,72 @@
 				$this->assign('cate',$cate);
 			}
 			if(IS_POST){
-				
-				/*$config = array(
-					'maxSize' => 3145728,
-					'savePath' => 'Uploads/img/',
-					'rootPath' => './Public/',
-					'saveName' => time().'_'.mt_rand(),
-					'exts' => array('jpg','jpeg','png','gif','rar','zip'),
-				);
-				$upload = upload($config);
-				//if($upload==true){
-					echo 1;var_dump($upload);
-					echo $upload[1]['savename'];exit;
-					$this->success('上传成功');			
-				//}else{
+				//var_dump($_FILES);
+				$files = array();
+				$files = $_FILES;
 
-					$this->error($upload);
-				//}*/
+				//将$_FILES数组的格式进行改变，变成单个作品的属性在同一数组下，在进行upload调用时，将文件单个进行上传
+				$_FILES = array();
+				//$n = count($files['picname']['name']);	
+				foreach($files as $key => $name){
+					//picname或compress上传的文件个数
+					$n = count($files[$key]['name']);
+					for($i=0;$i<$n;$i++){
+
+						$_FILES[$key][$i]['name'] = $name['name'][$i];
+						$_FILES[$key][$i]['type'] = $name['type'][$i];
+						$_FILES[$key][$i]['tmp_name'] = $name['tmp_name'][$i];
+						$_FILES[$key][$i]['error'] = $name['error'][$i];
+						$_FILES[$key][$i]['size'] = $name['size'][$i];
+						
+					}
+				}
+				//var_dump($_FILES);
+				//exit;
+				$n = I('post.pid');	//上传的文件子目录
+				//计算上传了几张图片
+				$inum = count($_FILES['picname']);
+				//计算上传几个压缩包
+				$cnum = count($_FILES['compress']);
+				//循环将图片单个上传
+				for($i=0;$i<$inum;$i++){
+					$config = array(
+					'maxSize' => 3145728,
+					'savePath' => 'Uploads/work/img/'.$n.'/',
+					'rootPath' => './Public/',
+					'saveName' => md5(uniqid(microtime(true),true)),
+					'exts' => array('jpg','jpeg','png','gif'),
+					);
+					$file = array();
+					$file[] = $_FILES['picname'][$i];
+					
+					//$config初始化上传信息,$file单个文件进行上传
+					$upload = upload($config,$file);
+					//得到图片的保存路径，用@and进行分隔路径，保存在数据库
+					$works .= '@and'.'./Public/'.$upload[0]['savepath'].$upload[0]['savename'];
+					$works = ltrim($works,'@and');
+				}
+				//循环将压缩包单个上传
+				for($i=0;$i<$cnum;$i++){
+					$config = array(
+					'maxSize' => 10*1024*1024,
+					'savePath' => 'Uploads/work/compress/'.$n.'/',
+					'rootPath' => './Public/',
+					'saveName' => md5(uniqid(microtime(true),true)),
+					'exts' => array('rar','zip'),
+					);
+					$file = array();
+					$file[] = $_FILES['compress'][$i];
+					$upload = upload($config,$file);
+					//得到压缩包保存的路径，多个文件之间的路径用@and分隔保存在数据库
+					$compress .= '@and'.'./Public/'.$upload[0]['savepath'].$upload[0]['savename'];
+					$compress = ltrim($compress,'@and');
+				}
+
+				//exit;
+				if(!$upload==true){
+					$this->error($upload);			
+				}
 				$data['workname'] = trim(I('post.workname'));
 				$data['cate_id'] = I('post.pid');
 				$data['intro'] = I('post.intro');
@@ -63,15 +112,18 @@
 						$this->error('输入的值不能为空');
 					}
 				}
+				//将输入的金币整型化
 				$data['price'] = (int)floor(I('post.price'));
+				$data['works'] = $works;
+				$data['compress'] = $compress;
 				if(!is_numeric($data['price'])){
 					$this->error('您输入的金币有误，请输入一个整数!!!');
 				}
-				//$this->assign('data',$data);
+				$data['user_id'] = 1;	//默认作者为管理员
 				$model = M('work');
 				$num = $model->add($data);
 				if($num){
-					$this->success('添加作品成功!');
+					$this->success('添加作品成功!',"index");
 				}else{
 					$this->error('添加作品失败！');
 				}
@@ -80,26 +132,31 @@
 		}
 
 		function verify($id){
+			$model = M('work');
+			$model2 = M('category');
 			if(!IS_POST){
-				$model = M('work');
-				$model2 = M('category');
+				
 				$cate = $model2->select();
 				$work = $model->alias('w')
 							  ->join('user u on w.user_id=u.id')
-							  ->field('workname,works,cate_id,price,compress,intro,u.username')
+							  ->field('w.id,workname,works,cate_id,price,intro,u.username')
 							  ->where('w.id='.$id)
 							  ->find();
+				$url = get_url($work['works']);
 				//$work['cate_id'] = (int)$work['cate_id'];
-				$compress = uncompress($work['compress']);
-				$this->assign('compress',$compress);
+				//$compress = uncompress($work['compress']);
+				//$this->assign('compress',$compress);
+				$this->assign('url',$url);
 				$this->assign('cate',$cate);
 				$this->assign('work',$work);
 
 			}
 			if(IS_POST){
+				$id = $_POST['id'];
 				$data['workname'] = trim(I('post.workname'));
 				$data['cate_id'] = I('post.pid');
 				$data['intro'] = I('post.intro');
+				$data['flag'] = I('post.flag');
 				foreach($data as $v){
 					if(strlen(trim($v))==0){
 						$this->error('输入的值不能为空');
@@ -109,12 +166,33 @@
 				if(!is_numeric($data['price'])){
 					$this->error('您输入的金币有误，请输入一个整数!!!');
 				}
+				$num = $model->where('id='.$id)->save($data);
+				if($num){
+					$this->success('操作成功!');
+				}else{
+					$this->error('操作失败!');
+				}
 			}
 			$this->display();
 		}
 
 		function edit($id){
-
+			if(!IS_POST){
+				$model = M('work');
+				$model2 = M('category');
+				$cate = $model2->select();
+				$work = $model->alias('w')
+							  ->join('user u on w.user_id=u.id')
+							  ->field('w.id,workname,works,cate_id,price,intro,u.username')
+							  ->where('w.id='.$id)
+							  ->find();
+				$this->assign('cate',$cate);
+				$this->assign('work',$work);
+			}
+			if(IS_POST){
+				$id = $_POST['id'];
+			}
+			$this->display();
 		}
 
 		function delete($id){
