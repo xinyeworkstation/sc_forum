03e12e0abf74d28bd705378e0fe27e12ddbd507a
zhugeyang <2119508463@qq.com>
2016-12-18 16:03:21 +0800

    前台整理好

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -2,27 +2,9 @@
 namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
-<<<<<<< HEAD
-    
-    public function index()
-    {
-=======
-    public function index(){
-    	$model = M('category');
-    	$model2 = M('work');
-    	$cate = $model->select();
-    	if(IS_POST){
-    		$id = $_POST['cate_id'];
-    		$work = $model2->alias('w')
-    					   ->join('user u ON w.user_id=u.id')
-    					   ->field('w.id,workname,works,u.username')
-    					   ->where('w.cate_id='.$id)
-    					   ->select();
-    		$this->assign('work',$work);
 
-    	}
-    	$this->assign('cate',$cate);
->>>>>>> 286018c8eba6442bd1ec252fffa2c662759a52bb
+    public function index(){
+    	
         $this->display();
     }
 
