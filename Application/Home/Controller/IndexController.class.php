1c8f7ad2672f9da5d37a7654786bfe55b7e93703
zyq <1378622586@qq.com>
2016-12-20 19:31:29 +0800

    index

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -4,7 +4,27 @@ use Think\Controller;
 class IndexController extends Controller {
 
     public function index(){
-    	
+    	if(!IS_POST){
+    		$model = M('work');
+    		$order = $model->alias('w')
+    					   ->join('user u ON u.id=w.user_id')
+    					   ->field('user_id,sum(download) down,u.username')
+    					   ->group('user_id')
+    					   ->order('down desc')
+    					   ->limit(10)
+    					   ->select();
+    		//var_dump($order);exit;
+    		$work = $model->alias('w')
+    					  ->join('user u ON w.user_id=u.id')
+    					  ->join('left join favorite f ON w.id=f.work_id')
+    					  ->field('w.id w_id,works,download,u.id u_id,u.username,u.headimg,count(f.work_id) favor')
+    					  ->group('f.work_id')
+    					  ->select();
+
+    		//var_dump($work);exit;
+    		$this->assign('order',$order);
+    		$this->assign('work',$work);
+    	}	
         $this->display();
     }
     
