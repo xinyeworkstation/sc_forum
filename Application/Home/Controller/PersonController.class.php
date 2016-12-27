50958bff62fe4a0488ceee8c6c46cea0c5e17349
zhugeyang <2119508463@qq.com>
2016-12-27 17:06:02 +0800

    123

--- Application/Home/Controller/PersonController.class.php
+++ Application/Home/Controller/PersonController.class.php
@@ -33,38 +33,49 @@ class PersonController extends Controller {
         $this->assign('work',$work);
         $this->display();
     }
+
+
      public function production(){
-        $flag=I('get.flag');
-        //flag 0禁用 1通过 2待审核 3不通过
-        $work=M('work');
-        $where['w.user_id']=1;
-        $work=$work->where($where)
-                    ->alias('w')
-                    ->field('w.flag,w.cate_id,w.works,w.workname,u.username,u.headimg,w.download,w.favor')
-                    ->join('user u on w.user_id=u.id')
-                    ->select();
-        $count=count($work);
-        $allow=0;
-        $undeter=0;
-        $ban=0;
-        for($i=0;$i<$count;$i++){
-            $work[$i]['headimg']='/'.$work[$i]['headimg'];//处理头像
-            $img=get_url($work[$i]['works']);//处理图片
-            $work[$i]['works']='/'.$img[0];
-            if(strcmp($work[$i]['flag'],'1')==0){
-                $allow++;
-            }elseif(strcmp($work[$i]['flag'],'2')==0){
-                $undeter++;
-            }else{
-                $ban++;
-            }
-        }
-        $this->assign('allow',$allow);
-        $this->assign('undeter',$undeter);
-        $this->assign('ban',$ban);
-        $this->assign('img',$img);
-        $this->assign('work',$work);
-        $this->display();
+	     	//var_dump($_GET);
+	        $getflag=I('get.flag');
+	        //flag 0禁用 1通过 2待审核 3不通过,要将相应的地方的作品放到相应的地方
+	        $work=M('work');
+	        $id=1;
+	        $where['w.user_id']=1;
+	       	$where['w.flag']=$getflag;
+	        $allow=0;
+	        $undeter=0;
+	        $ban=0;
+	        $flag=$work->where('user_id='.$id)
+	                    ->field('flag')
+	                    ->select();
+	        $flagc=count($flag);
+	        for($i=0;$i<$flagc;$i++){
+	            if(strcmp($flag[$i]['flag'],'1')==0){
+	                $allow++;
+	            }elseif(strcmp($flag[$i]['flag'],'2')==0){
+	                $undeter++;
+	            }elseif(strcmp($flag[$i]['flag'],'3')==3){
+	                $ban++;
+	            }
+	        }
+
+	        $work=$work->where($where)
+	                    ->alias('w')
+	                    ->field('w.flag,w.cate_id,w.works,w.workname,u.username,u.headimg,w.download,w.favor')
+	                    ->join('user u on w.user_id=u.id')
+	                    ->select();
+	        $count=count($work);
+	        for($i=0;$i<$count;$i++){
+	            $work[$i]['headimg']='/'.$work[$i]['headimg'];//处理头像
+	            $img=get_url($work[$i]['works']);//处理图片
+	            $work[$i]['works']='/'.$img[0];
+	        }
+	        $this->assign('allow',$allow);
+	        $this->assign('undeter',$undeter);
+	        $this->assign('ban',$ban);
+	        $this->assign('work',$work);
+	        $this->display();
     }
      public function transationRecord(){
         $user=M('user');
