7acfc90e399ead48867ed0574fdae29b4b75ef46
yangjiaixng <975289275@qq.com>
2016-12-30 19:20:47 +0800

    123

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -14,4 +14,15 @@ return array(
     'LAYOUT_ON'=>false,
     'URL_MODEL'             =>2,
 
+
+    // 配置邮件发送服务器
+    'MAIL_HOST' =>' smtp.163.com',//smtp服务器的名称
+    'MAIL_SMTPAUTH' =>TRUE, //启用smtp认证
+    'MAIL_USERNAME' =>'13245002606@163.com',//你的邮箱名
+    'MAIL_FROM' =>'13245002606@163.com',//发件人地址
+    'MAIL_FROMNAME'=>'阳嘉兴',//发件人姓名
+    'MAIL_PASSWORD' =>'yangjiaxing521',//邮箱密码
+    'MAIL_CHARSET' =>'utf-8',//设置邮件编码
+    'MAIL_ISHTML' =>TRUE, // 是否HTML格式邮件
+
 );
