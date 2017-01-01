dcffe5937e93c2b09bf3ecf849aa242505e2417b
yangjiaixng <975289275@qq.com>
2017-01-01 20:52:50 +0800

    阳

--- Application/Admin/Controller/RechargeController.class.php
+++ Application/Admin/Controller/RechargeController.class.php
@@ -27,7 +27,7 @@ class RechargeController extends BaseController
         $count = $model->where($where)->count();// 查询满足要求的总记录数
         $Page = new \Extend\Page($count, 15);// 实例化分页类 传入总记录数和每页显示的记录数(25)
         $show = $Page->show();// 分页显示输出
-        $post1 = $model->limit($Page->firstRow . ',' . $Page->listRows)->JOIN('user ON  user.id = recharge.user_id  ')->where($where)->order('recharge.id DESC')->field('recharge.money,recharge.time,recharge.id,user.username')->select();
+        $post1 = $model->limit($Page->firstRow . ',' . $Page->listRows)->JOIN('user ON  user.id = recharge.user_id  ')->where($where)->order('recharge.id DESC')->field('recharge.money,recharge.time,recharge.id,user.username,recharge.order_number')->select();
         //print_r($post1);
         //$post = $model->limit($Page->firstRow . ',' . $Page->listRows)->where($where)->order('id DESC')->select();
         $this->assign('model', $post1);
